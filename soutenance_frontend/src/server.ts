f7cff34a0e89f0a5550728c604bc0ded36b632c4
fatou-dieye <fatoujeey2001@example.com>
2025-05-26 14:41:57 +0000

    mise à jour

--- soutenance_frontend/src/server.ts
+++ soutenance_frontend/src/server.ts
@@ -1,3 +1,4 @@
+/*
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
@@ -29,7 +30,7 @@ const angularApp = new AngularNodeAppEngine();
 /**
  * Serve static files from /browser
  */
-app.use(
+/*app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
     index: false,
@@ -40,7 +41,7 @@ app.use(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use('/**', (req, res, next) => {
+/*app.use('/**', (req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
@@ -53,7 +54,7 @@ app.use('/**', (req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
+/*if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
@@ -63,4 +64,5 @@ if (isMainModule(import.meta.url)) {
 /**
  * The request handler used by the Angular CLI (dev-server and during build).
  */
-export const reqHandler = createNodeRequestHandler(app);
+/*export const reqHandler = createNodeRequestHandler(app);
+*/
